84dfc9217ac808e47d2a7dad6f1a4b95a85a99c3
Dominika <01168920@pw.edu.pl>
2023-11-12 00:40:10 +0100

    added make_ribbon_plot but not working

--- main.py
+++ main.py
@@ -3,28 +3,35 @@ from cec2017.functions import f1, f9
 import matplotlib.pyplot as plt
 import random
 
-
-def one_fifth_success_rule(successful_mutations, i, sigma, adaptation_interval):
-    if i % adaptation_interval == 0:
-        if successful_mutations / adaptation_interval > 1 / 5:
-            sigma *= 1.22
-        elif successful_mutations / adaptation_interval < 1 / 5:
-            sigma *= 0.82
-        successful_mutations = 0
-    return sigma, successful_mutations
-
-
 def make_plot(results):
-    for idx, result in enumerate(results):
+    for idx, result in enumerate(results[1:]):
         plt.plot(
             [i for i in range(0, result.num_of_iterations + 1)],
-            result.min_func_value,
+            result.func_values,
             label=f"Result {idx+1}",
         )
+    plt.yscale('log')
+    plt.xscale('log')
     plt.xlabel("Iteration")
     plt.ylabel("Value of the objective function")
     plt.title("Plot of the objective function values from iterations")
     plt.legend()
+    plt.show()
+    
+def make_ribbon_plot(results):
+    fig, ax = plt.subplots()
+
+    for idx, result in enumerate(results[1:]):
+        iterations = np.arange(result.num_of_iterations + 1)
+        func_values = result.func_values
+        ax.fill_between(iterations, min(func_values), max(func_values), alpha=0.2, label=f"Result {idx+1}")
+
+    ax.set_yscale('log')
+    ax.set_xscale('log')
+    ax.set_xlabel("Iteration")
+    ax.set_ylabel("Value of the objective function")
+    ax.set_title("Ribbon Plot of the objective function values from iterations")
+    ax.legend()
     plt.show()
 
 
@@ -37,9 +44,8 @@ class optim_result:
 
 
 class params_t:
-    def __init__(self, sigma, adaptation_interval, max_iter):
+    def __init__(self, sigma, max_iter):
         self.sigma = sigma
-        self.adaptation_interval = adaptation_interval #wyjebać
         self.max_iter = max_iter
 
 
@@ -63,55 +69,57 @@ class one_fifth_succ_rule:
 # ile licz sprawdzamy
 
 class Evolutionary:
-    def __init__(self, function, x0, params):
+    def __init__(self, function, x0, params, stepsize_adaptation_policy):
         self.f = function
         self.params = params
-        self.successful_mutation = 0
-        self.result = optim_result(x0, function(x0), 0, [function(x0)])
+        self.successful_mutations = 0
+        self.result = optim_result(x0, function(x0), params.max_iter, [function(x0)])
         self.population_size = len(x0)
+        self.stepsize_adaptation_policy = stepsize_adaptation_policy
 
 
-def mutate(reproduction, population, mutation_rate):
-    return [population[i] + mutation_rate * reproduction[i] for i in range(len(population))]
-    # return np.sum(population + np.multiply(reproduction, mutation_rate))
-    # numpy sum i multiply
+    def mutate(self, reproduction, mutation_rate):
+        return [self.result.minimum[i] + mutation_rate * reproduction[i] for i in range(self.population_size)]
+        # return np.sum(population + np.multiply(reproduction, mutation_rate))
+        # numpy sum i multiply
 
-
-
-def evolution_algorithm(
-    func, population, params: params_t, stepsize_adaptation_policy=0
-) -> optim_result:
-    population_size = len(population)
-    y_min_tab = [func(population)]
-    y_min = func(population)
-    min_pop = population
-    successful_mutations = 0
-    for i in range(params.max_iter):
-        random_point = [random.gauss(0, 1) for _ in range(population_size)]
-        mut_population = mutate(random_point, population, params.sigma)
-        # y_min, x_min = update_min_point(mut_population)
-        #to dać jako update_new_minumum - w klasie
-        zmienna = func(mut_population)
-        if y_min >= zmienna:
-            successful_mutations += 1
-            y_min = func(mut_population)
-            min_pop = mut_population
-            population = mut_population
-        successful_mutations, params.sigma = stepsize_adaptation_policy.update_mutation_stren(successful_mutations, i, params.sigma)
-        y_min_tab.append(y_min)
-    return optim_result(min_pop, y_min_tab, params.max_iter)
+    def update_min_point(self, mut_population):
+        zmienna = self.f(mut_population)
+        if self.result.min_func_value >= zmienna:
+            self.successful_mutations += 1
+            self.result.min_func_value = self.f(mut_population)
+            self.result.minimum = mut_population
+        self.result.func_values.append(self.result.min_func_value)
+         
+            
+    def evolution_algorithm(self) -> optim_result:
+        for i in range(self.params.max_iter):
+            random_point = [random.gauss(0, 1) for _ in range(self.population_size)]
+            mut_population = self.mutate(random_point, self.params.sigma)
+            self.update_min_point(mut_population)
+            self.successful_mutations, self.params.sigma = self.stepsize_adaptation_policy.update_mutation_stren(self.successful_mutations, i, self.params.sigma)
+        # return optim_result(self.result.minimum, self.result.min_func_value, params.max_iter)
 
 
 if __name__ == "__main__":
     x0 = np.array([1.0, 1.0, 1.0, 1.0, 2.0, 1.0, 1.0, 1.0, 1.0, 1.0])
     stepsize_adapt_policy = one_fifth_succ_rule(30.0)
-    params_t = params_t(10.0, 20.0, 100000)
+    params = params_t(10.0, 100000)
+    obiekty =[]
     #range max 10
+    suma = 0
     for _ in range(10):
-        results = evolution_algorithm(
-            f1, population=x0, params=params_t, stepsize_adaptation_policy=stepsize_adapt_policy
+        eval = Evolutionary(
+            f1, x0, params, stepsize_adapt_policy
         )
-        print(_, "Minimum dla f1:", results.minimum, "Wartość funkcji celu:", results.min_func_value[-1])
+        eval.evolution_algorithm()
+        # make_plot(eval.result)
+        obiekty.append(eval.result)
+        suma += eval.result.min_func_value
+        print(_, "Minimum dla f1:", eval.result.minimum, "Wartość funkcji celu:", eval.result.min_func_value)
+    print("Średnia: ", suma/10)
+    make_plot(obiekty)
+    make_ribbon_plot(obiekty)
         # make_plot(results)
     # print("wartosc funkcji celu:", result)
 #rysować wykres funkcji czy średnią?
