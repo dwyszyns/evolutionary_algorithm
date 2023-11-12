47acea7d69ebfb88c7832ef383ed09d695cec08d
Dominika <01168920@pw.edu.pl>
2023-11-12 18:35:12 +0100

    added ribbon plot but only one simulation

--- main.py
+++ main.py
@@ -19,13 +19,14 @@ def make_plot(results):
     plt.show()
     
 def make_ribbon_plot(results):
-    fig, ax = plt.subplots()
-
-    for idx, result in enumerate(results[1:]):
-        iterations = np.arange(result.num_of_iterations + 1)
-        func_values = result.func_values
-        ax.fill_between(iterations, min(func_values), max(func_values), alpha=0.2, label=f"Result {idx+1}")
-
+    _, ax = plt.subplots()
+    all_func_values = np.array(results[1:])
+    min_func_values = np.min(all_func_values, axis=0)
+    max_func_values = np.max(all_func_values, axis=0)
+    mean_func_values = np.mean(all_func_values, axis=0)
+    iterations = np.arange(len(results[0]))
+    ax.fill_between(iterations, min_func_values, max_func_values, alpha=0.2)
+    ax.plot(iterations, mean_func_values, color='b', alpha=0.7)
     ax.set_yscale('log')
     ax.set_xscale('log')
     ax.set_xlabel("Iteration")
@@ -35,6 +36,19 @@ def make_ribbon_plot(results):
     plt.show()
 
 
+class symulation_results:
+    def __init__(self):
+        self.optim_results = []
+        self.sum = 0
+        
+    def mean(self):
+        return self.sum/len(self.optim_results)
+    
+    def add_new_result(self, optim_result):
+        self.optim_results.append(optim_result)
+        self.sum += optim_result[-1]
+    
+
 class optim_result:
     def __init__(self, minimum, min_func_value, num_of_iterations=1, func_values=[]):
         self.minimum = minimum
@@ -42,12 +56,10 @@ class optim_result:
         self.num_of_iterations = num_of_iterations
         self.func_values = func_values
 
-
 class params_t:
     def __init__(self, sigma, max_iter):
         self.sigma = sigma
         self.max_iter = max_iter
-
 
 class one_fifth_succ_rule:
     def __init__(self, adaptation_interval):
@@ -62,11 +74,6 @@ class one_fifth_succ_rule:
                 mutation_stren *= 0.82
             successful_mutations = 0
         return successful_mutations, mutation_stren
-    
-
-# można dodać parametr, który sprawdza średnią
-# epsilon
-# ile licz sprawdzamy
 
 class Evolutionary:
     def __init__(self, function, x0, params, stepsize_adaptation_policy):
@@ -84,8 +91,7 @@ class Evolutionary:
         # numpy sum i multiply
 
     def update_min_point(self, mut_population):
-        zmienna = self.f(mut_population)
-        if self.result.min_func_value >= zmienna:
+        if self.result.min_func_value >= self.f(mut_population):
             self.successful_mutations += 1
             self.result.min_func_value = self.f(mut_population)
             self.result.minimum = mut_population
@@ -98,30 +104,17 @@ class Evolutionary:
             mut_population = self.mutate(random_point, self.params.sigma)
             self.update_min_point(mut_population)
             self.successful_mutations, self.params.sigma = self.stepsize_adaptation_policy.update_mutation_stren(self.successful_mutations, i, self.params.sigma)
-        # return optim_result(self.result.minimum, self.result.min_func_value, params.max_iter)
 
 
 if __name__ == "__main__":
     x0 = np.array([1.0, 1.0, 1.0, 1.0, 2.0, 1.0, 1.0, 1.0, 1.0, 1.0])
     stepsize_adapt_policy = one_fifth_succ_rule(30.0)
     params = params_t(10.0, 100000)
-    obiekty =[]
-    #range max 10
-    suma = 0
-    for _ in range(10):
-        eval = Evolutionary(
-            f1, x0, params, stepsize_adapt_policy
-        )
+    wyniczki = symulation_results()
+    for _ in range(3):
+        eval = Evolutionary(f1, x0, params, stepsize_adapt_policy)
         eval.evolution_algorithm()
-        # make_plot(eval.result)
-        obiekty.append(eval.result)
-        suma += eval.result.min_func_value
+        wyniczki.add_new_result(eval.result.func_values)
         print(_, "Minimum dla f1:", eval.result.minimum, "Wartość funkcji celu:", eval.result.min_func_value)
-    print("Średnia: ", suma/10)
-    make_plot(obiekty)
-    make_ribbon_plot(obiekty)
-        # make_plot(results)
-    # print("wartosc funkcji celu:", result)
-#rysować wykres funkcji czy średnią?
-
-#wywalic population size
+    print("Średnia: ", wyniczki.mean())
+    make_ribbon_plot(wyniczki.optim_results)
