cf56ff6f17749a3349d24c2dcbd77996b7c320e3
Dominika <01168920@pw.edu.pl>
2023-11-15 10:24:21 +0100

    done :)

--- main.py
+++ main.py
@@ -4,25 +4,22 @@ import matplotlib.pyplot as plt
 import random
 
 
-def make_plot(results):
-    labels = ['adapt_interval = 0.005', 'adapt_interval = 1', 'adapt_interval = 50', 'adapt_interval = 1000']
+def make_normal_plot(results):
     for i in range(len(results)):
         all_func_values = np.array(results[i][1:])
         mean_func_values = np.mean(all_func_values, axis=0)
         iterations = np.arange(len(results[i][0]))
-        plt.plot(iterations, mean_func_values)
+        plt.plot(iterations, mean_func_values, label=f"Result {i+1}")
     plt.yscale("log")
-    # plt.xscale("log")
+    plt.xscale("log")
     plt.xlabel("Iteration")
     plt.ylabel("Value of the function")
-    plt.title("Plot of the function values from iterations")
     plt.legend()
-    plt.savefig("wykres_norm_f9_max_iter.png")
-    plt.show()  
+    plt.title("Plot of the function values from iterations")
+    plt.show()
 
 
 def make_ribbon_plot(results):
-    labels =['adapt_interval = 0.005', 'adapt_interval = 1', 'adapt_interval = 50', 'adapt_interval = 1000']
     _, ax = plt.subplots()
     for i in range(len(results)):
         all_func_values = np.array(results[i][1:])
@@ -30,31 +27,23 @@ def make_ribbon_plot(results):
         max_func_values = np.max(all_func_values, axis=0)
         mean_func_values = np.mean(all_func_values, axis=0)
         iterations = np.arange(len(results[i][0]))
-        ax.fill_between(
-            iterations, min_func_values, max_func_values, alpha=0.2
-        )
-        ax.plot(iterations, mean_func_values,  alpha=0.7)
+        ax.fill_between(iterations, min_func_values, max_func_values, alpha=0.2)
+        ax.plot(iterations, mean_func_values, alpha=0.7, label=f"Result {i+1}")
     ax.set_yscale("log")
-    # ax.set_xscale("log")
+    ax.set_xscale("log")
     ax.set_xlabel("Iteration")
     ax.set_ylabel("Value of the function")
     ax.set_title("Ribbon Plot of the function values from iterations")
-    ax.legend()
-    plt.savefig("ribbon_f9_max_iter.png")
-    plt.show()  
+    plt.legend()
+    plt.show()
 
 
 class simulation_results:
     def __init__(self, length):
         self.optim_results = [[] for _ in range(length)]
-        self.sum = [0] * length #wywalić przy oddaniu
-
-    def symulation_mean(self, i): #wywalić przy oddaniu
-        return self.sum[i] / len(self.optim_results[i])
 
     def add_new_result(self, optim_result, i):
         self.optim_results[i].append(optim_result)
-        self.sum[i] += optim_result[-1]
 
 
 class optim_result:
@@ -76,7 +65,7 @@ class one_fifth_succ_rule:
         self.adapt_interval = adapt_interval
         self.target_prob = 1 / 5
 
-    def update_mutation_stren(self, successful_mutations, i, mutation_stren) -> float:
+    def update_mutation_stren(self, successful_mutations, i, mutation_stren):
         if i % self.adapt_interval == 0:
             if successful_mutations / self.adapt_interval > self.target_prob:
                 mutation_stren *= 1.22
@@ -87,11 +76,10 @@ class one_fifth_succ_rule:
 
 
 class Evolution_Solver:
-    def __init__(self, function, x0, params, mut_stren_adapt_policy):
-        self.f = function
+    def __init__(self, x0, params, mut_stren_adapt_policy):
         self.params = params
         self.success_mutations = 0
-        self.result = optim_result(x0, function(x0), params.max_iter, [function(x0)])
+        self.result = optim_result(x0, 0, params.max_iter, [])
         self.point_size = len(x0)
         self.mut_stren_adapt_policy = mut_stren_adapt_policy
 
@@ -101,42 +89,43 @@ class Evolution_Solver:
             for i in range(self.point_size)
         ]
 
-    def update_min_point(self, mut_point):
-        if self.result.min_func_value >= self.f(mut_point):
+    def update_min_point(self, f_mut_pint, mut_point):
+        if self.result.min_func_value >= f_mut_pint:
             self.success_mutations += 1
-            self.result.min_func_value = self.f(mut_point)
+            self.result.min_func_value = f_mut_pint
             self.result.min_point = mut_point
         self.result.func_values.append(self.result.min_func_value)
 
-    def solver(self):
+    def solve(self, f):
+        self.result.min_func_value = f(self.result.min_point)
+        self.result.func_values.append(self.result.min_func_value)
         for i in range(self.params.max_iter):
             random_point = [random.gauss(0, 1) for _ in range(self.point_size)]
             mut_point = self.mutate(random_point, self.params.mutation_stren)
-            self.update_min_point(mut_point)
+            self.update_min_point(f(mut_point), mut_point)
             (
                 self.success_mutations,
                 self.params.mutation_stren,
             ) = self.mut_stren_adapt_policy.update_mutation_stren(
                 self.success_mutations, i, self.params.mutation_stren
             )
-            
 
 
 if __name__ == "__main__":
     x0 = np.array([1.0, 1.0, 1.0, 1.0, 2.0, 1.0, 1.0, 1.0, 1.0, 1.0])
     mut_stren_adapt_policy = one_fifth_succ_rule(10.0)
-    # must_stren_list = [one_fifth_succ_rule(0.005), one_fifth_succ_rule(1.0), one_fifth_succ_rule(50.0), one_fifth_succ_rule(1000.0)]
-    # params_list = [params_t(0.1, 10000), params_t(0.1, 10000), params_t(0.1, 10000), params_t(0.1, 10000)]
-    params_list = [params_t(10.0, 10000)]
+    params_list = [
+        params_t(0.0001, 10000),
+        params_t(0.1, 10000),
+        params_t(1, 10000),
+        params_t(1000, 10000),
+    ]
     results = simulation_results(len(params_list))
     for i in range(len(params_list)):
         for _ in range(10):
-            eval = Evolution_Solver(f1, x0, params_list[0], mut_stren_adapt_policy)
-            eval.solver()
-            results.add_new_result(eval.result.func_values, i)
-            print(_,"Minimum dla f1:",eval.result.min_point,"Wartość funkcji celu:",eval.result.min_func_value)#wywalić do oddania
-        print("Średnia: ", results.symulation_mean(i)) #wywalić do oddania
-
+            evolution_solver = Evolution_Solver(
+                x0, params_list[0], mut_stren_adapt_policy
+            )
+            evolution_solver.solve(f1)
+            results.add_new_result(evolution_solver.result.func_values, i)
     make_ribbon_plot(results.optim_results)
-    make_plot(results.optim_results)
-
